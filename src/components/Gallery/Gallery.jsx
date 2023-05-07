13797a95e03aa0247b4d457ba22da221788c356a
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-07 11:43:36 +0700

    Remove "Show More/Less" and "YouTubeAudio"

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,28 +1,13 @@
 import React, { useState, useEffect } from "react";
 import characters from "../../assets/characters.json";
-import YouTubeAudio from "../YouTubeAudio/YouTubeAudio";
 import "./Gallery.css";
 
-const Gallery = ({ onCharSelect, selectedCharID, videoID }) => {
-    const [showAll, setShowAll] = useState(false);
+const Gallery = ({ onCharSelect, selectedCharID }) => {
     const initialDisplayCount = 7;
-
-    const handleImageClick = (charID) => {
-        onCharSelect(charID);
-    };
-
-    const handleShowMore = () => {
-        setShowAll(true);
-    };
-
-    const handleShowLess = () => {
-        setShowAll(false);
-    };
 
     const circularIndex = (index, length) => (index + length) % length;
 
     const getDisplayedCharacters = () => {
-        if (showAll) return characters;
         if (!selectedCharID) return characters.slice(0, initialDisplayCount);
 
         const selectedIndex = characters.findIndex(
@@ -37,16 +22,12 @@ const Gallery = ({ onCharSelect, selectedCharID, videoID }) => {
 
     const displayedCharacters = getDisplayedCharacters();
 
-
-
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (event.key === "ArrowDown") {
                 event.preventDefault();
-                handleShowMore();
             } else if (event.key === "ArrowUp") {
                 event.preventDefault();
-                handleShowLess();
             }
         };
 
@@ -57,14 +38,12 @@ const Gallery = ({ onCharSelect, selectedCharID, videoID }) => {
         };
     }, []);
 
-
+    const handleImageClick = (charID) => {
+        onCharSelect(charID);
+    };
 
     return (
         <div className="gallery-container">
-            <div className={`youtube-audio-container ${showAll ? "visible" : "hidden"}`}>
-                <YouTubeAudio videoID={videoID} />
-            </div>
-
             <div className="gallery-side">
                 {displayedCharacters.map((character) => (
                     <div
@@ -76,15 +55,6 @@ const Gallery = ({ onCharSelect, selectedCharID, videoID }) => {
                         <img src={character.profileImg} alt={character.name} />
                     </div>
                 ))}
-            </div>
-
-            <div className="button-side">
-                <button
-                    className="show-button"
-                    onClick={showAll ? handleShowLess : handleShowMore}
-                >
-                    {showAll ? "Show Less" : "Show More"}
-                </button>
             </div>
         </div>
     );
