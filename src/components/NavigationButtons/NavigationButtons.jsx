930438f330765a4b713d8a758e7426421f4c3fb4
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-10 14:26:08 +0700

    Background Music

--- src/components/NavigationButtons/NavigationButtons.jsx
+++ src/components/NavigationButtons/NavigationButtons.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./NavigationButtons.css";
 
 const NavigationButtons = ({ goToPreviousSlide, handleCharSelect, handleCharDeselect, selectedCharacters, goToNextSlide }) => {
+    const [bgmPlaying, setBgmPlaying] = useState(false);
+    const [audio, setAudio] = useState(null);
+
+
+
+    const toggleBackgroundMusic = () => {
+        if (bgmPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        setBgmPlaying(!bgmPlaying);
+    };
+
+    const handleBgmToggle = () => {
+        toggleBackgroundMusic();
+    };
+
+
+
+    useEffect(() => {
+        const bgmAudio = new Audio('./bgm.mp3');
+        bgmAudio.loop = true;
+        setAudio(bgmAudio);
+
+        return () => {
+            bgmAudio.pause();
+            bgmAudio.currentTime = 0;
+        };
+    }, []);
+
+
+
     return (
         <div className="navigation-buttons">
             <button onClick={goToPreviousSlide} className="left-button">
@@ -9,6 +42,9 @@ const NavigationButtons = ({ goToPreviousSlide, handleCharSelect, handleCharDese
             </button>
             <button onClick={handleCharSelect} disabled={selectedCharacters.length >= 3}>
                 Select
+            </button>
+            <button onClick={handleBgmToggle}>
+                {bgmPlaying ? 'Pause BGM' : 'Play BGM'}
             </button>
             <button onClick={handleCharDeselect} disabled={selectedCharacters.length === 0}>
                 Deselect
