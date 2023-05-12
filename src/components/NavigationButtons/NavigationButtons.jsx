9a328a5b56e9fae62dbf36ef875480b8204d5123
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-12 18:24:43 +0700

    Video button fix

--- src/components/NavigationButtons/NavigationButtons.jsx
+++ src/components/NavigationButtons/NavigationButtons.jsx
@@ -10,23 +10,17 @@ const NavigationButtons = ({
     scrollToRandomCharacter,
 }) => {
 
-    // State to track whether the background music (BGM) is currently playing
     const [bgmPlaying, setBgmPlaying] = useState(false);
-    // State to store the audio object for the BGM
     const [audio, setAudio] = useState(null);
-    // State to track video playback
     const [videoPlaying, setVideoPlaying] = useState(true);
 
     // Function to toggle the background music playback
     const toggleBackgroundMusic = () => {
         if (bgmPlaying) {
-            // Pause the BGM if it is currently playing
             audio.pause();
         } else {
-            // Play the BGM if it is currently paused
             audio.play();
         }
-        // Toggle the BGM playing state
         setBgmPlaying(!bgmPlaying);
     };
 
@@ -40,6 +34,7 @@ const NavigationButtons = ({
     // Function to toggle video playback
     const toggleVideoPlayback = () => {
         setVideoPlaying(!videoPlaying);
+        document.activeElement.blur();
     };
 
 
@@ -47,13 +42,10 @@ const NavigationButtons = ({
     useEffect(() => {
         // Create a new Audio object for the background music
         const bgmAudio = new Audio('./audio/bgm.mp3');
-        // Set the loop property to true to continuously play the BGM
         bgmAudio.loop = true;
-        // Set the audio object in the state
         setAudio(bgmAudio);
 
         return () => {
-            // Clean up the audio object when the component unmounts
             bgmAudio.pause();
             bgmAudio.currentTime = 0;
         };
@@ -64,35 +56,28 @@ const NavigationButtons = ({
     return (
         <div className="navigation-buttons">
             <div className="navigation-buttons-row">
-                {/* Button to go to the previous slide */}
                 <button onClick={goToPreviousSlide} className="left-button">
                     ◄
                 </button>
-                {/* Button to handle character selection */}
                 <button onClick={handleCharSelect} disabled={selectedCharacters.length >= 3}>
                     Select
                 </button>
-                {/* Button to toggle video playback */}
                 <button onClick={toggleVideoPlayback}>
                     Video
                 </button>
                 <button onClick={scrollToRandomCharacter}>
                     Random
                 </button>
-                {/* Button to toggle the background music */}
                 <button onClick={handleBgmToggle}>
                     BGM
                 </button>
-                {/* Button to handle character deselection */}
                 <button onClick={handleCharDeselect} disabled={selectedCharacters.length === 0}>
                     Deselect
                 </button>
-                {/* Button to go to the next slide */}
                 <button onClick={goToNextSlide} className="right-button">
                     ►
                 </button>
             </div>
-            {/* Video element with conditional playback */}
             {videoPlaying && (
                 <video className="background-video" autoPlay loop muted>
                     <source src="./background.mp4" type="video/mp4" />
